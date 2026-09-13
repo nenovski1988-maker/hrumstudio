093c14794f641557c969940c463ded0143df3ff3
nenovski1988-maker <nenovski1988@gmail.com>
2026-09-13 13:17:04 +0300

    add shotfactory to sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -6,5 +6,11 @@ export default function sitemap() {
       changeFrequency: "monthly",
       priority: 1,
     },
+    {
+      url: "https://www.hrumstudio.online/products/shotfactory",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ];
 }
